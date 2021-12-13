eeb2329ecf24baebfc0182bea6bec50d6b09067c
EC2 Default User <ec2-user@ip-10-0-0-105.ap-northeast-1.compute.internal>
2021-12-13 14:26:19 +0000

    fix cors routes

--- dbserver.js
+++ dbserver.js
@@ -1,26 +1,25 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const cors = require('cors');
+const routes = require('./routes/contact.routes');
 const app = express();
 var corsOptions = {
 	origin: "http://localhost:4200"
 }
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
 const db = require('./models');
 db.sequelize.sync();
 
 // default route
-app.get('/', function(req, res) {
-	return res.send({ message: 'Welcome to AddressBook Backend' });
-});
+//app.get('/', function(req, res, next) {
+//	return res.send({ message: 'Welcome to AddressBook Backend' });
+//});
 
-require('./routes/contact.routes')(app);
+app.use('/api/contacts', routes);
 const PORT = process.env.PORT || 9080;
 app.listen(PORT, () => {
 	console.log(`Server listening on: ${PORT}`);
--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,18 +1,22 @@
-module.exports = app => {
-	const contacts = require('../controllers/contact.controller.js');
-	var router = require('express').Router();
-	// Create Contact
-	router.post('/', contacts.create);
-	// Retrieve All Contacts
-	router.get('/', contacts.findAll);
-	// Retrieve Contact by Id
-	router.get('/:id', contacts.findById);
-	// Update Contact
-	router.put('/:id', contacts.update);
-	// Delete Contact
-	router.delete('/:id', contacts.delete);
-	// Delete All Contacts
-	router.delete('/', contacts.deleteAll);
+const express = require('express');
+const cors=require('cors');
+const contacts = require('../controllers/contact.controller.js');
+var router = express.Router();
+var corsOptions = {
+        origin: "http://localhost:4200"
+}
+router.use(cors(corsOptions));
+// Create Contact
+router.post('/', contacts.create);
+// Retrieve All Contacts
+router.get('/', contacts.findAll);
+// Retrieve Contact by Id
+router.get('/:id', contacts.findById);
+// Update Contact
+router.put('/:id', contacts.update);
+// Delete Contact
+router.delete('/:id', contacts.delete);
+// Delete All Contacts
+router.delete('/', contacts.deleteAll);
 
-	app.use('/api/contacts', router);
-};
+module.exports = router;
